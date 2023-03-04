44e3ac74d4b66438b110f0bac093a0d43ed95974
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-03-04 22:24:46 +0900

    feat(config): videoのpreloadは不要 / 不要なwasmプラグインを削除

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,10 @@
-import path from 'node:path';
-
 import react from '@vitejs/plugin-react';
 import { visualizer as analyzer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 import { ViteEjsPlugin } from 'vite-plugin-ejs';
 import topLevelAwait from 'vite-plugin-top-level-await';
-import wasm from 'vite-plugin-wasm';
-
-import { getFileList } from './tools/get_file_list';
-
-const publicDir = path.resolve(__dirname, './public');
-const getPublicFileList = async (targetPath: string) => {
-  const filePaths = await getFileList(targetPath);
-  const publicFiles = filePaths
-    .map((filePath) => path.relative(publicDir, filePath))
-    .map((filePath) => path.join('/', filePath));
-
-  return publicFiles;
-};
 
 export default defineConfig(async ({ mode }) => {
-  const videos = await getPublicFileList(path.resolve(publicDir, 'videos'));
-
   return {
     build: {
       assetsInlineLimit: 4096,
@@ -52,12 +35,10 @@ export default defineConfig(async ({ mode }) => {
     },
     plugins: [
       react(),
-      wasm(),
       topLevelAwait(),
       ViteEjsPlugin({
         module: '/src/client/index.tsx',
         title: '買えるオーガニック',
-        videos,
       }),
     ],
   };
