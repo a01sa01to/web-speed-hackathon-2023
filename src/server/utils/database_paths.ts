2e545c9f3b2707ad29c0d19b82c26b25b0a85ca3
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-03-07 13:24:24 +0900

    Revert "feat: Deploy on GAE"
    This reverts commit 4ea374c387981ffa5e21e89248f46bbcf5a29478.

--- src/server/utils/database_paths.ts
+++ src/server/utils/database_paths.ts
@@ -1,5 +1,4 @@
 import { rootResolve } from './root_resolve';
 
-export const DATABASE_PATH =
-  process.env.IS_GAE === 'true' ? '/tmp/database.sqlite' : rootResolve('databases/database.sqlite');
+export const DATABASE_PATH = rootResolve('databases/database.sqlite');
 export const DATABASE_SEED_PATH = rootResolve('databases/database.seed.sqlite');
